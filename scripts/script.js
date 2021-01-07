962f63c038f8d3374b5368ebf7f774bb38817cf5
Balou van Iterson <balouviterson@gmail.com>
2021-01-07 01:06:51 +0100

    3rd page + Javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 // JavaScript Document
+
+deP = document.querySelector("p.Eerste");
+
+deP2 = document.querySelector("p.Tweede");
+
+deP3 = document.querySelector("p.pmeer");
+
+
+
+deP3.addEventListener("click", ietsDoen);
+
+
+function ietsDoen() {
+    deP.classList.add("hide");
+    deP2.classList.add("show");
+    deP3.classList.add("move")
+}
